89ee09ed1a602d997fee9a1eb69a8f0d277a73c4
lilianluca <lilianluca2118@gmail.com>
2024-04-12 19:15:13 +0200

    Added some tests

---
+++ tests/test_my_math.py
@@ -0,0 +1,13 @@
+import src.my_math as my_math
+
+
+def test_add1():
+    assert my_math.add(1, 2) == 3
+
+
+def test_add2():
+    assert my_math.add(5, 7) == 12
+
+
+def test_add3():
+    assert my_math.add(100, -100) == 0
